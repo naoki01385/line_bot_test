920707c3fc52ece0fdc3f62ffada3f2d43c16f8f
naoki01385 <89433218+naoki01385@users.noreply.github.com>
2021-09-23 00:06:57 +0900

    Update main.py

--- Jupyter/line_bot/main.py
+++ Jupyter/line_bot/main.py
@@ -1,10 +1,10 @@
 import json
 
-file=open('info.json','r')
-info=json.load(file)
-
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
+
+file=open('info.json','r')
+info=json.load(file)
 
 CHANNEL_ACCESS_TOKEN=info['CHANNEL_ACCESS_TOKEN']
 line_bot_api=LineBotApi(CHANNEL_ACCESS_TOKEN)
